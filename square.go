79b3825cef5884b61ac8e8feb06a2b560e98f019
Darya Lamaka <lomakodasha@gmail.com>
2022-06-06 13:12:09 +0200

    fix: work of End method

--- square.go
+++ square.go
@@ -9,17 +9,17 @@ type Square struct {
 	a     uint
 }
 
-func (square *Square) End() Point {
+func (square Square) End() Point {
 	sideLength := int(square.a)
 	square.start.x = square.start.x + sideLength
-	square.start.x = square.start.x + sideLength
+	square.start.y = square.start.y + sideLength
 	return square.start
 }
 
-func (square *Square) Area() uint {
+func (square Square) Area() uint {
 	return square.a * square.a
 }
 
-func (square *Square) Perimeter() uint {
+func (square Square) Perimeter() uint {
 	return square.a * 4
 }
